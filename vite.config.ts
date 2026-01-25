2b4a8050a9f20bb0f00322dac24ab550292f675c
lukasz.gruszka.ch <lukasz.gruszka.ch@gmail.com>
2026-01-25 08:29:42 +0000

    added pwa definition

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,38 @@
-import {defineConfig} from 'vite'
-import { resolve } from 'path';
+import {defineConfig} from 'vite';
+import {resolve} from 'path';
+import {VitePWA} from 'vite-plugin-pwa';
 
 export default defineConfig({
+  plugins: [
+    VitePWA({
+      manifest: {
+        display: 'standalone',
+        name: 'Status Bar',
+        short_name: 'Status Bar',
+        background_color: '#4c4c4c',
+        theme_color: '#4c4c4c',
+        display_override: ['window-controls-overlay'],
+        icons: [
+          {
+            src: './icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         settings: resolve(__dirname, 'settings.html'),
+      },
+      preserveEntrySignatures: 'strict',
+      output: {
+        // Has to be false to avoid firebase error:
+        // "Service database is not available".
+        preserveModules: false,
       },
     },
   },
